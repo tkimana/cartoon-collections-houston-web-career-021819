f96d87fffe84a691d6fa7546cc3e7d0ae54e6c70
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-11 17:57:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(name,array){
+if ["Doc","Dopey","Bashful","Grumpy"]
+}else ["Dad","Repair"]
+    
+  end
+
+def summon_captain_planet
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
